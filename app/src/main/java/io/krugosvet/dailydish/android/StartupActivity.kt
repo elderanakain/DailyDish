b683c3fadde6706856dc3fc5003c3bab5a2acacf
Arcadii Rubailo <rubailo.arcadii@gmail.com>
2017-12-06 00:52:59 +0100

    Initial commit

---
+++ app/src/main/java/io/krugosvet/dailydish/android/StartupActivity.kt
@@ -0,0 +1,12 @@
+package io.krugosvet.dailydish.android
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class StartupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_startup)
+    }
+}
